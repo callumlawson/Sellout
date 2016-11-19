e9280862c4a6f8dc6c7011f6cd537ce1815096b2
Caryn Krakauer <caryn@krakauer.org>
2016-11-19 13:38:47 +0000

    UI sprites and drink state

---
+++ Assets/Scripts/States/DrinkState.cs
@@ -0,0 +1,33 @@
+﻿using Assets.Framework.States;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts.States
+{
+    [Serializable]
+    class DrinkState : IState
+    {
+        public enum Ingredient
+        {
+            Alcohol
+        };
+
+        [SerializeField] private Dictionary<Ingredient, int> contents;
+
+        public DrinkState(Dictionary<Ingredient, int> contents)
+        {
+            this.contents = contents;
+        }
+
+        public override string ToString()
+        {
+            var output = "Drink: \n";
+            foreach (var content in contents)
+            {
+                output = output + "\t(" + content.Key + "->" + content.Value + ")\n";
+            }
+            return output;
+        }
+    }
+}
