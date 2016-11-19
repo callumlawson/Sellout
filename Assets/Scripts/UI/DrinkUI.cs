4c3bf2c3b0f05376ba4344477072d0b2705114dc
Caryn Krakauer <caryn@krakauer.org>
2016-11-19 20:11:18 +0000

    ingredient panel
    fonts

--- Assets/Scripts/UI/DrinkUI.cs
+++ Assets/Scripts/UI/DrinkUI.cs
@@ -1,14 +1,29 @@
-﻿using UnityEngine;
+﻿using Assets.Scripts.UI;
+using Assets.Scripts.Util;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
 
 namespace Assets.Scripts.Systems
 {
     class DrinkUI : MonoBehaviour
     {
+#pragma warning disable 649
+        public IngredientPanelUI ingredientTemplate;
+        public RectTransform ingredientListParent;
+#pragma warning restore 649
+
         public delegate void OnMixEvent();
         public delegate void OnCloseEvent();
+        public delegate void OnIncrementIngredient(Ingredient ingredient);
+        public delegate void OnDecrementIngredient(Ingredient ingredient);
 
         public OnMixEvent onMixEvent;
         public OnCloseEvent onCloseEvent;
+        public OnIncrementIngredient onIncrementIngredient;
+        public OnDecrementIngredient onDecrementIngredient;
+
+        private Dictionary<Ingredient, IngredientPanelUI> ingredientPanels;
 
         public void Mix()
         {
@@ -18,6 +33,34 @@ namespace Assets.Scripts.Systems
         public void Close()
         {
             onCloseEvent();
+        }
+
+        public void IncrementIngredient(Ingredient ingredient)
+        {
+            if (onIncrementIngredient != null)
+            {
+                onIncrementIngredient(ingredient);
+            }
+        }
+
+        public void DecrementIngredient(Ingredient ingredient)
+        {
+            if (onDecrementIngredient != null)
+            {
+                onDecrementIngredient(ingredient);
+            }
+        }
+
+        public void Awake()
+        {
+            ingredientPanels = new Dictionary<Ingredient, IngredientPanelUI>();
+            foreach (Ingredient ingredient in Enum.GetValues(typeof(Ingredient)))
+            {
+                var panel = Instantiate(ingredientTemplate);
+                panel.Initialize(ingredient, ingredientListParent);
+                
+                ingredientPanels.Add(ingredient, panel);
+            }
         }
     }
 }
