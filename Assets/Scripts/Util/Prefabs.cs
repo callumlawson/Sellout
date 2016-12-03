daa2d8eb809f560c8139e27b9b417d7241e7aedc
Caryn Krakauer <caryn@krakauer.org>
2016-12-03 18:36:37 +0000

    fix bad merge

--- Assets/Scripts/Util/Prefabs.cs
+++ Assets/Scripts/Util/Prefabs.cs
@@ -4,22 +4,14 @@
     {
         public const string Person = "Person";
         public static string Booth = "Furnature/Booth";
-<<<<<<< HEAD
         public const string Waypoint = "Waypoint";
         public const string Drink = "Drinks/Drink";
         public const string Counter = "Environment/Counter";
         public const string Player = "Player";
 
         //UI
-        public const string DrinkMixingUI = "UI/DrinkMixingUI";
+        public const string DrinkMixingUI = "UI/DrinkMixing/DrinkMixingUI";
         public const string DiagloguePanelUI = "UI/DialoguePanel";
         public const string DialogueLineUI = "UI/DialogueLine";
-=======
-        public static string Waypoint = "Waypoint";
-        public static string Drink = "Drinks/Drink";
-        public static string DrinkMixingUI = "UI/DrinkMixing/DrinkMixingUI";
-        public static string Counter = "Environment/Counter";
-        public static string Player = "Player";
->>>>>>> mixologyBook
     }
 }
