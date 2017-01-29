861a27ac4ce9b390f2d13ed23a180778c2102b2f
Caryn Krakauer <caryn@krakauer.org>
2017-01-29 21:52:18 +0000

    Dialogue UI will always fit on the screen even if there is a small resolution.

---
+++ Assets/Scripts/UI/ScreenFitter.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.UI
+{
+    class ScreenFitter : MonoBehaviour
+    {
+        private float margin = 50;
+
+        protected void Start()
+        {
+            var panel = GetComponent<RectTransform>();
+            var newWidth = Mathf.Min(Screen.width - margin, panel.sizeDelta.x);
+            var newHeight = Mathf.Min(Screen.height - margin, panel.sizeDelta.y);
+
+            panel.sizeDelta = new Vector2(newWidth, newHeight);
+        }
+    }
+}
