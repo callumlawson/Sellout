a9e30799856c5f335ce71aea977270c7f3ce5a77
Caryn Krakauer <caryn@krakauer.org>
2017-01-29 22:07:47 +0000

    * MixologyBook now fits onto screen no matter what.
    * Text can fade out as well as fade in. Title and day text now does so.
    * Moved clock to upper left so it doesn't interfere with the mixology book.

--- Assets/Scripts/Util/TextFadeIn.cs
+++ Assets/Scripts/Util/TextFadeIn.cs
@@ -1,5 +1,6 @@
 ﻿using DG.Tweening;
 using JetBrains.Annotations;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,8 +8,10 @@ namespace Assets.Scripts.Util
 {
     class TextFadeIn : MonoBehaviour
     {
-        [UsedImplicitly]
-        public float DurationSeconds = 8.0f;
+        [UsedImplicitly] public float DurationSeconds = 8.0f;
+
+        [UsedImplicitly] public bool DoFadeOut;
+        [UsedImplicitly] public int FadeOutWaitTime;
 
         [UsedImplicitly]
         public void Start()
@@ -17,6 +20,21 @@ namespace Assets.Scripts.Util
             text.DOFade(0.0f, 0.0f);
             text.enabled = true;
             text.DOFade(1.0f, DurationSeconds).SetEase(Ease.InCubic);
+
+            if (DoFadeOut)
+            {
+                StartCoroutine(FadeOut());
+            }
+        }
+
+        private IEnumerator FadeOut()
+        {
+            yield return new WaitForSeconds(DurationSeconds + FadeOutWaitTime);
+            var text = GetComponent<Text>();
+            text.DOFade(1.0f, 1.0f);
+            text.DOFade(0.0f, DurationSeconds).SetEase(Ease.InCubic);
+            yield return new WaitForSeconds(DurationSeconds);
+            text.enabled = false;
         }
     }
 }
