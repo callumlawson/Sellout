90b93f652ef6d64ddd9d370689873a429b8b1228
Callum Lawson <lawsonathome@gmail.com>
2017-04-03 22:03:48 +0100

    Async sceen loading.

--- Assets/Scripts/Util/SceneSwitcher.cs
+++ Assets/Scripts/Util/SceneSwitcher.cs
@@ -1,14 +1,35 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using JetBrains.Annotations;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class SceneSwitcher : MonoBehaviour {
 
     public string DestinationScene;
 
+    private AsyncOperation gameLoading;
+
+    [UsedImplicitly]
+    public void Start()
+    {
+        StartCoroutine(LoadScene());
+    }
+
+    IEnumerator LoadScene()
+    {
+        gameLoading = SceneManager.LoadSceneAsync(DestinationScene);
+        gameLoading.allowSceneActivation = false;
+
+        Debug.Log("start loading");
+
+        yield return gameLoading;
+    }
+
+    [UsedImplicitly]
 	void Update () {
 		if(Input.GetMouseButtonDown(0))
-        {
-            SceneManager.LoadScene(DestinationScene, LoadSceneMode.Single);
-        }
+		{
+		    gameLoading.allowSceneActivation = true;
+		}
 	}
 }
