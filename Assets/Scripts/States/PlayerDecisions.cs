1b6654d9d4ce407eade90d5d4822d37c81fa5cb2
Callum AF. Lawson <callum@improbable.io>
2017-04-08 23:28:43 +0100

    Missing file. Volume adjustment.

---
+++ Assets/Scripts/States/PlayerDecisions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Assets.Framework.States;
+
+namespace Assets.Scripts.States
+{
+    [Serializable]
+    public class PlayerDecisions : IState
+    {
+        public bool AcceptedDrugPushersOffer;
+        public bool ToldInspectorAboutDrugPusher;
+    }
+}
