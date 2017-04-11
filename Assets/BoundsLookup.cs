36159d1d8499034701933290b12e591135fa822e
Callum AF. Lawson <callum@improbable.io>
2017-04-11 20:56:00 +0200

    Fixed name tags showing while outside bar.

---
+++ Assets/BoundsLookup.cs
@@ -0,0 +1,20 @@
+﻿using JetBrains.Annotations;
+using UnityEngine;
+
+public class BoundsLookup : MonoBehaviour
+{
+    public static BoundsLookup Instance;
+
+    public BoxCollider BarBounds;
+
+	[UsedImplicitly]
+	void Start ()
+	{
+	    Instance = this;
+	}
+
+    public Bounds GetBarBounds()
+    {
+        return BarBounds.bounds;
+    }
+}
