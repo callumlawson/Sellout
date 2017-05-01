286b0d0699523958c2f106125b16d574caa642d2
Caryn Krakauer <caryn@improbable.io>
2017-05-01 21:56:28 +0100

    Bar console has on and off screens.

---
+++ Assets/Scripts/Visualizers/Bar/BarConsoleVisualizer.cs
@@ -0,0 +1,32 @@
+﻿using Assets.Framework.States;
+using Assets.Scripts.States;
+using UnityEngine;
+
+namespace Assets.Scripts.Visualizers.Bar
+{
+    public class BarConsoleVisualizer : MonoBehaviour
+    {
+        public GameObject ScreenOn;
+        public GameObject ScreenOff;
+
+        void Start()
+        {
+            var dayPhaseState = StaticStates.Get<DayPhaseState>();
+            dayPhaseState.DayPhaseChangedTo += DayPhaseChangedTo;
+        }
+
+        private void DayPhaseChangedTo(DayPhase newPhase)
+        {
+            if (newPhase == DayPhase.Open)
+            {
+                ScreenOn.SetActive(true);
+                ScreenOff.SetActive(false);
+            }
+            else
+            {
+                ScreenOn.SetActive(false);
+                ScreenOff.SetActive(true);
+            }
+        }
+    }
+}
