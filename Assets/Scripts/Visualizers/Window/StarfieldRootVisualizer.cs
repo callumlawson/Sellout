99901e09e90f6aa80d45990f1b6d90d8dd90a0d0
Caryn Krakauer <caryn@improbable.io>
2017-06-09 20:56:56 +0100

    Stars follow the players so they look further away in the window.

---
+++ Assets/Scripts/Visualizers/Window/StarfieldRootVisualizer.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using Assets.Framework.Entities;
+using Assets.Framework.States;
+using Assets.Scripts.States;
+
+public class StarfieldRootVisualizer : MonoBehaviour
+{
+    GameObject player;
+
+    void Start()
+    {
+        var particles = GameObject.FindGameObjectsWithTag("Starfield");
+        foreach (var particle in particles)
+        {
+            var system = particle.GetComponent<ParticleSystem>();
+            var pm = system.main;
+            pm.simulationSpace = ParticleSystemSimulationSpace.Custom;
+            pm.customSimulationSpace = transform;
+            system.Stop();
+            system.Clear();
+            system.Play();
+        }
+    }
+    
+    void Update()
+    {
+        if (player == null)
+        {
+            var playerState = StaticStates.Get<PlayerState>();
+            if (playerState != null)
+            {
+                player = playerState.Player.GameObject;
+            }
+
+            if (player == null) return;
+        }
+
+        this.transform.position = player.transform.position;
+    }
+}
