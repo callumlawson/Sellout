65a4eb42b41b58c474e22356852c68407bce431b
Callum Lawson <lawsonathome@gmail.com>
2017-06-13 00:12:51 +0100

    Fixed minor UI issue that cause dialog background not to stretch the full length of the box.

--- Assets/Scripts/UI/ResponseMouseOver.cs
+++ Assets/Scripts/UI/ResponseMouseOver.cs
@@ -1,23 +1,28 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using DG.Tweening;
+using JetBrains.Annotations;
 
 namespace Assets.Scripts.UI
 {
-    class ResponseMouseOver : MonoBehaviour
+    [UsedImplicitly]
+    public class ResponseMouseOver : MonoBehaviour
     {
         private Image backgroundImage;
 
+        [UsedImplicitly]
         void Start()
         {
             backgroundImage = GetComponent<Image>();
         }
 
+        [UsedImplicitly]
         public void OnMouseEnter()
         {
-            backgroundImage.DOFade(0.25f, 0.0f);
+            backgroundImage.DOFade(0.25f, 0.1f);
         }
 
+        [UsedImplicitly]
         public void OnMouseLeave()
         {
             backgroundImage.DOFade(0.0f, 0.1f);
