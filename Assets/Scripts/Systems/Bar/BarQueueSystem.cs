d6a401cf87ba7e1e9f6179f64fcdfc0a59d29773
Caryn Krakauer <caryn@improbable.io>
2017-06-25 22:14:00 +0100

    Ability to queue up speical characters in preparation for fancier drug story.

--- Assets/Scripts/Systems/Bar/BarQueueSystem.cs
+++ Assets/Scripts/Systems/Bar/BarQueueSystem.cs
@@ -10,13 +10,28 @@ using Assets.Scripts.GameActions.Waypoints;
 using Assets.Scripts.States;
 using Assets.Scripts.Systems.AI;
 using Assets.Scripts.Util;
-using UnityEngine;
+using Assets.Scripts.GameActions.Composite;
 
 namespace Assets.Scripts.Systems.Bar
 {
     public class BarQueueSystem : IInitSystem, ITickSystem, IReactiveEntitySystem
     {
+        public static BarQueueSystem Instance;
+
+        private class EntityActionsPair
+        {
+            public Entity entity;
+            public ActionSequence actions;
+
+            public EntityActionsPair(Entity entity, ActionSequence actions)
+            {
+                this.entity = entity;
+                this.actions = actions;
+            }
+        }
+
         private TimeState time;
+        private PlayerState playerState;
         private Entity player;
 
         private Entity purchaseWaypoint;
@@ -25,15 +40,29 @@ namespace Assets.Scripts.Systems.Bar
         private readonly HashSet<Entity> allCharacters = new HashSet<Entity>();
 
         private readonly HashSet<Entity> inUseCharacters = new HashSet<Entity>();
-        private readonly HashSet<Entity> specialCharacters = new HashSet<Entity>();
+
+        private readonly LinkedList<Entity> specialCharacters = new LinkedList<Entity>();
+        private readonly Dictionary<Entity, ActionSequence> specialCharacterActions = new Dictionary<Entity, ActionSequence>();
+
+        public BarQueueSystem()
+        {
+            Instance = this;
+        }
 
         public void OnInit()
         {
             time = StaticStates.Get<TimeState>();
-            player = StaticStates.Get<PlayerState>().Player;
+            playerState = StaticStates.Get<PlayerState>();
+            player = playerState.Player;
 
             var dayPhase = StaticStates.Get<DayPhaseState>();
             dayPhase.DayPhaseChangedTo += DayPhaseChangedTo;
+        }
+
+        public void QueueEntityNext(Entity entity, ActionSequence actions)
+        {
+            specialCharacters.AddLast(entity);
+            specialCharacterActions.Add(entity, actions);
         }
 
         private void DayPhaseChangedTo(DayPhase phase)
@@ -42,10 +71,13 @@ namespace Assets.Scripts.Systems.Bar
             {
                 inUseCharacters.Clear();
                 specialCharacters.Clear();
+                specialCharacterActions.Clear();
 
                 if (time.GameTime.GetDay() == 1)
                 {
-                    specialCharacters.Add(allCharacters.First(entity => entity.GetState<NameState>().Name == "Q"));
+                    var q = allCharacters.First(entity => entity.GetState<NameState>().Name == "Q");
+                    specialCharacters.AddLast(q);
+                    specialCharacterActions.Add(q, DrugStory.DrugPusherIntro(q));
                 }
                 else if (time.GameTime.GetDay() == 2)
                 {
@@ -75,7 +107,7 @@ namespace Assets.Scripts.Systems.Bar
 
         public void Tick()
         {
-            if (StaticStates.Get<DayPhaseState>().CurrentDayPhase != DayPhase.Open)
+            if (StaticStates.Get<DayPhaseState>().CurrentDayPhase != DayPhase.Open || playerState.PlayerStatus != PlayerStatus.Bar)
             {
                 return;
             }
@@ -103,14 +135,32 @@ namespace Assets.Scripts.Systems.Bar
             var purchaseWaypointIsFree = purchaseWaypoint.GetState<UserState>().IsFree();
             var waitForPurchaseWaypointIsFree = waitForPurchaseWaypoint.GetState<UserState>().IsFree();
 
-            if (purchaseWaypointIsFree)
+            if (specialCharacters.Count > 0)
             {
-                OnPurchaseWaypointFree();
-            }
+                if (purchaseWaypointIsFree)
+                {
+                    var availableCharacter = specialCharacters.FirstOrDefault(entity => ActionManagerSystem.Instance.IsEntityIdle(entity));
+                    if (availableCharacter != null)
+                    {
+                        ActionManagerSystem.Instance.QueueAction(availableCharacter, new TeleportAction(waitForPurchaseWaypoint.GameObject.transform.position));
+                        AddPurchaseActionsForCharacter(availableCharacter, specialCharacterActions[availableCharacter]);
 
-            if (waitForPurchaseWaypointIsFree)
+                        specialCharacters.Remove(availableCharacter);
+                        specialCharacterActions.Remove(availableCharacter);
+                    }
+                }
+            }
+            else
             {
-                OnWaitForPurchaseWaypointFree();
+                if (purchaseWaypointIsFree)
+                {
+                    OnPurchaseWaypointFree();
+                }
+
+                if (waitForPurchaseWaypointIsFree)
+                {
+                    OnWaitForPurchaseWaypointFree();
+                }
             }
 
             if (GetNextCharacter() == null && purchaseWaypoint.GetState<UserState>().IsFree() && waitForPurchaseWaypoint.GetState<UserState>().IsFree())
@@ -135,29 +185,25 @@ namespace Assets.Scripts.Systems.Bar
 
             waitForPurchaseWaypoint.GetState<UserState>().ClearReserver();
             waitForPurchaseWaypoint.GetState<UserState>().ClearUser();
+
+            var purchaseSequence = new ActionSequence("Purchase");
+            purchaseSequence.Add(DrinkOrders.GetRandomOrder(waitingCharacter));
+            purchaseSequence.Add(CommonActions.SitDown());
+            purchaseSequence.Add(CommonActions.SitDownLoop());
             
-            AddPurchaseActionsForCharacter(waitingCharacter);
+            AddPurchaseActionsForCharacter(waitingCharacter, purchaseSequence);
         }
 
-        private void AddPurchaseActionsForCharacter(Entity character)
+        private void AddPurchaseActionsForCharacter(Entity character, ActionSequence sequence)
         {
             character.GetState<ActionBlackboardState>().TargetEntity = purchaseWaypoint;
             purchaseWaypoint.GetState<UserState>().Reserve(character, "Bar Queue System");
             purchaseWaypoint.GetState<UserState>().Use(character, "Bar Queue System");
             ActionManagerSystem.Instance.QueueAction(character, new GoToWaypointAction());
 
-            if (time.GameTime.GetDay() == 1 && character.GetState<NameState>().Name == "Q")
-            {
-                ActionManagerSystem.Instance.QueueAction(character, DrugStory.DrugPusherIntro(character));
-            }
-            else
-            {   
-                ActionManagerSystem.Instance.QueueAction(character, DrinkOrders.GetRandomOrder(character));
-                ActionManagerSystem.Instance.QueueAction(character, CommonActions.SitDown());
-                ActionManagerSystem.Instance.QueueAction(character, CommonActions.SitDownLoop());
-            }
+            ActionManagerSystem.Instance.QueueAction(character, sequence);
         }
-
+        
         private void OnWaitForPurchaseWaypointFree()
         {
             var nextCharacter = GetNextCharacter();
@@ -180,24 +226,11 @@ namespace Assets.Scripts.Systems.Bar
         {
             var freeCharacters = new HashSet<Entity>(allCharacters);
             freeCharacters.ExceptWith(inUseCharacters);
-            var idleCharacters = freeCharacters.Where(entity => ActionManagerSystem.Instance.IsEntityIdle(entity)).ToList();
+            var idleCharacters = freeCharacters.Where(entity => ActionManagerSystem.Instance.IsEntityIdle(entity)).Where(entity => !specialCharacters.Contains(entity)).ToList();
 
             if (idleCharacters.Count == 0)
             {
                 return null;
-            }
-
-            if (specialCharacters.Count > 0)
-            {
-                var choice = specialCharacters.First();
-
-                if (!idleCharacters.Contains(choice))
-                {
-                    Debug.LogError("Want to use special character " + choice + " but that character is busy!");
-                }
-
-                specialCharacters.Remove(choice);
-                return choice;
             }
 
             var randomChoice = UnityEngine.Random.Range(0, idleCharacters.Count - 1);
