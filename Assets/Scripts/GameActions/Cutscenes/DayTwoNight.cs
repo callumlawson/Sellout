5d468546eccd20a992f8f044af7208090894cc90
Callum Lawson <lawsonathome@gmail.com>
2017-07-13 22:48:59 +0100

    Changes in progress.

--- Assets/Scripts/GameActions/Cutscenes/DayTwoNight.cs
+++ Assets/Scripts/GameActions/Cutscenes/DayTwoNight.cs
@@ -17,6 +17,7 @@ namespace Assets.Scripts.GameActions.Cutscenes
            
             var mcGraw = EntityQueries.GetEntityWithName(matchingEntities, NPCS.McGraw.Name);
             var jannet = EntityQueries.GetEntityWithName(matchingEntities, NPCS.Jannet.Name);
+            var ellie = EntityQueries.GetEntityWithName(matchingEntities, NPCS.Ellie.Name);
             var q = EntityQueries.GetEntityWithName(matchingEntities, NPCS.Q.Name);
             var player = EntityQueries.GetEntityWithName(matchingEntities, "You");
 
@@ -35,7 +36,14 @@ namespace Assets.Scripts.GameActions.Cutscenes
             ActionManagerSystem.Instance.QueueAction(jannet, jannetSequence);
 
             //McGraw and Q
-            DrugStory.DrugPusherInspectorShowdown(mcGraw, q, Locations.SitDownPoint2());            
+            DrugStory.DrugPusherInspectorShowdown(mcGraw, q, Locations.SitDownPoint2());  
+            
+            //Ellie
+            var ellieSequence = new ActionSequence("Ellie night two");
+            ellieSequence.Add(new TeleportAction(Locations.SitDownPoint1()));
+            ellieSequence.Add(new SetConversationAction(new EllieMorningOne(), ellie));
+            ellieSequence.Add(CommonActions.SitDownLoop());
+            ActionManagerSystem.Instance.QueueAction(ellie, ellieSequence);
         }
 
         private class JannetNightTwo : Conversation
@@ -57,6 +65,30 @@ namespace Assets.Scripts.GameActions.Cutscenes
                     DialogueSystem.Instance.WriteNPCLine("How Q managed to find a market for his horrible substances I'll never know.");
                     DialogueSystem.Instance.WritePlayerChoiceLine("You'd be suprised. Being stuck in this space can drives people crazy eventually.", EndConversation(DialogueOutcome.Nice));
                     DialogueSystem.Instance.WritePlayerChoiceLine("You'd think in a ship as small as this there would be no room for it.", EndConversation(DialogueOutcome.Nice));
+                }
+            }
+        }
+
+        private class EllieNightTwo: Conversation
+        {
+
+
+            protected override void StartConversation(string converstationInitiator)
+            {
+                var decision = StaticStates.Get<PlayerDecisionsState>();
+                DialogueSystem.Instance.StartDialogue("Ellie");
+                if (ellie)
+                {
+                    DialogueSystem.Instance.WriteNPCLine("Have you seen Q? He owes me a, err, drink...");
+                    DialogueSystem.Instance.WritePlayerChoiceLine("Well if you need a 'drink' I'm here every day.", EndConversation(DialogueOutcome.Nice));
+                    DialogueSystem.Instance.WritePlayerChoiceLine("You shouldn't mix with Q, he's trouble.", EndConversation(DialogueOutcome.Mean));
+                }
+                else
+                {
+                    DialogueSystem.Instance.WriteNPCLine("What a bust up!");
+                    DialogueSystem.Instance.WriteNPCLine("How Q managed to find a market for his horrible substances I'll never know.");
+                    DialogueSystem.Instance.WritePlayerChoiceLine("You'd be suprised. Being stuck in this space can drives people crazy eventually.", EndConversation(DialogueOutcome.Nice));
+                    DialogueSystem.Instance.WritePlayerChoiceLine("You'd think in a ship as small as this there would be no room for it.", EndConversation(DialogueOutcome.Mean));
                 }
             }
         }
